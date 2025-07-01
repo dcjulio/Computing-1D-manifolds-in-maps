a3d9767dd588701b5e170eb24f04dc72ec103c5a
Dana C'Julio <107659476+dcjulio@users.noreply.github.com>
2025-07-01 14:16:46 +1200

    Add files via upload

--- GrowFundCurv1D_code/GrowFundCurv1D_demo.m
+++ GrowFundCurv1D_code/GrowFundCurv1D_demo.m
@@ -1,37 +1,41 @@
 %% Adding the path
-clear all
-addpath('./GrowFundCurv1D_functions');
+    clear all
+    close all
+    addpath('./GrowFundCurv1D_functions');
 
 %% Options
-%--- Loading and saving the initial segment of manifold
-load('pmin_a4.2_b0.3_xi1.20.mat')
-opts.init_segment=struct('x',data_x,'y',data_y,'z',data_z);
 
-%--- Information of the system
-opts.thesystem=StdHenon3D; %what is the name of the class with the info of the diffeo
-opts.par=struct('a', 4.2,'b', 0.3, 'xi', 1.2); %the parameter values
-opts.name_fixpoint='pmin'; % fixed point associated to the manifold
-opts.branch='pos'; %what side of the manifold I am computing. It is just a name. Examples: 'neg', 'pos', ''. 
+    %--- Information of the system
+    opts.thesystem=StdHenon3D; % What is the name of the system file
+    opts.par=struct('a', 4.2,'b', 0.3, 'xi', 1.2); % The parameter values and names (has to match with the names defined in StdHenon3D)
+    opts.user_arclength=2000; % What is the approximate arclength of the manifold
+    
+    %--- Number of iterations used to compute the manifold
+    opts.max_funditer=60; % how many times (max) the algorithm iterates the fundamental domain
 
-%--- Number of iterations used to compute the manifold
-opts.funditer=6; %whow many times the algorithm iterates the fundamental domain (multiple of mapiter).
-opts.mapiter=2; % how many iterations of the map are used (if non-orientable has to be even)
+    %--- Accuracy parameters (default)
+    %opts.accpar.alphamax=0.3;
+    %opts.accpar.deltalphamax=0.001; 
+    %opts.accpar.deltamin=0.000001;
+    %opts.accpar.deltamax=0.01;  
 
-%--- Accuracy parameters (default)
-%opts.accpar.alphamax=0.3;
-%opts.accpar.deltalphamax=0.001; 
-%opts.accpar.deltamin=0.000001;
-%opts.accpar.deltamax=0.01;    
+    %--- Initial step (default)
+    %opts.accpar.init_step=10^-7; % pos value is positive branch and viceversa
 
-%% Initializing the manifold structure
-manif = init_manif(opts);
 
-%% Computing the manifold
-manif=GrowFundCurv1D(manif,opts);
+%% Computing the manifold: Ws(pmin) orientation-preserving
+    opts.name_fixpoint='pmin'; % name of the fixed point associated to the manifold (has to match with the names defined in StdHenon3D)
+    opts.stability='Smanifold';
+
+    opts.branch='pos'; %which branch (Optional. Could be done with the sign of the init_step instead)
+    manif=GrowFundCurv1D(opts);
+
+    %% Computing the other branch
+    manif=add_branch(manif, opts, 'neg');
 
 %% Computing intersection points
-opts.angle=-3*pi/4; %the angle of the plane from [-pi, pi]. (angle=pi/2: x==0 (y>0), angle=0: y==0 (x>0))
-manif=inter_plane(manif,opts);
+    angle=-3*pi/4; %the angle of the plane from [-pi, pi]. (angle=pi/2: x==0 (y>0), angle=0: y==0 (x>0))
+    manif=inter_plane(manif,angle);
 
-%% Plotting the data
-f1=manifplot(manif);
+%% Plot
+    manifplot(manif);
